72435f9a8c862b68b0d9cdc6591be89071249782
Alex West <alexanderblakewest@gmail.com>
2026-07-03 14:32:04 -0500

    Add ArcGIS provider skeleton

--- app.js
+++ app.js
@@ -284,7 +284,7 @@ const utilitySources = [
         id: 'osm',
         name: 'OpenStreetMap',
         enabled: true,
-        query: queryOsmUtilities
+        query: runUtilitySources
     }
 
     // Future example:
